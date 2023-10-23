fd4489cfad4e1392b0f6f36315908445b8b18091
JHutch89 <JasonCHutches@gmail.com>
2023-10-23 10:17:21 -0400

    Creating dataframe options file

---
+++ df_options.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+# This is allowing me to show all columns/column headers
+pd.set_option('display.max_columns')
+# This is allowing me to show all rows
+pd.set_option('display.max_rows')
+# This is allowing me to keep all columns on one line
+pd.set_option('expand_frame_repr', False)
