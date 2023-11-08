8eb71f738b820b1ebe257256d98ededd020600e1
JHutch89 <JasonCHutches@gmail.com>
2023-11-07 22:13:21 -0500

    updated epl variable with dropped columns

--- EPL_Stats.py
+++ EPL_Stats.py
@@ -4,5 +4,7 @@ import df_options
 df_options.options()
 
 epl = pd.read_csv('/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/CSVs/premier_league_all_matches.csv')
+columns_to_drop = ['Home_xG', 'Away_xG']
+epl = epl.drop(columns = columns_to_drop)
 
 print(epl.head())
