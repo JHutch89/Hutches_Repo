a8e3233f0496e3e929a67dcbec68fcf172ce611e
JHutch89 <77797274+JHutch89@users.noreply.github.com>
2023-11-11 22:22:27 -0500

    Delete cols.py
    Deleted empty file

--- cols.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-
-# Read CSV file with explicit encoding
-epl = pd.read_csv('/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/CSVs/EPL_Data.csv')
-
-cols = epl.columns.to_list()
-
-print(cols)
