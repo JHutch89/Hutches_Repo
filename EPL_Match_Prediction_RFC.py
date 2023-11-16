caaa67b3156fbbf8f5df6bf576aef71393c0df6d
JHutch89 <77797274+JHutch89@users.noreply.github.com>
2023-11-15 21:50:10 -0500

    Update EPL_Match_Prediction_RFC.py

--- EPL_Match_Prediction_RFC.py
+++ EPL_Match_Prediction_RFC.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score  # Import accuracy_score
+from sklearn.metrics import accuracy_score
 
 # Read CSV
 epl = pd.read_csv('CSVs/EPL_Data.csv')
