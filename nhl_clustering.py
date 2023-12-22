668b9974cb3f03832b63d9d52018bbc9d55e2a33
JHutch89 <JasonCHutches@gmail.com>
2023-12-22 10:39:30 -0500

    Added elbow method to review for optimal K

--- nhl_clustering.py
+++ nhl_clustering.py
@@ -27,23 +27,38 @@ X = nhl_data.drop('playerId', axis=1)  # Exclude playerId for clustering
 scaler = StandardScaler()
 X_scaled = scaler.fit_transform(X)
 
-# Finding optimal PCA components
+# PCA 
 pca = PCA()
 pca.fit(X_scaled)
 
-# Get component stats
+## PCA stats
 component_stats = pd.DataFrame({
     'Component': range(1, pca.n_components_ + 1),
     'Explained Variance Ratio': pca.explained_variance_ratio_,
     'Cumulative Explained Variance': pca.explained_variance_ratio_.cumsum()
 })
 
+## pushing PCA stats to a csv
 component_stats.to_csv('/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/CSVs/NHL_pca_stats.csv', index=False)
 
+#--------------------------Uncomment if you want to plot PCA---------------------------------------------------
+## plotting PCA results
 # # Plotting the explained variance ratio to help choose the number of components
 # plt.plot(range(1, len(pca.explained_variance_ratio_) + 1), pca.explained_variance_ratio_.cumsum(), marker='o')
 # plt.xlabel('Number of Components')
 # plt.ylabel('Cumulative Explained Variance')
 # plt.show()
+
+## assigning components
+optimal_components = 2
+pca = PCA(n_components=optimal_components)
+X_pca = pca.fit_transform(X_scaled)
+
+## elbow method for optimal K
+inertia = []
+for k in range (1,11): ## update thise depending on number of features
+    kmeans = KMeans(n_clusters=k, random_state=42)
+    kmeans.fit(X_pca)
+    inertia.append(kmeans.inertia_)
 
 
