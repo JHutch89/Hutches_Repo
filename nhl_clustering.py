b3216ac6978a87cf8a2f5acba8c453bcb64b699e
JHutch89 <JasonCHutches@gmail.com>
2023-12-22 16:23:54 -0500

    applied KMeans clustering and optimal K

--- nhl_clustering.py
+++ nhl_clustering.py
@@ -4,6 +4,8 @@ from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
 from sklearn.pipeline import make_pipeline
 import matplotlib.pyplot as plt
+from sklearn.metrics import silhouette_score
+from sklearn.preprocessing import StandardScaler
 
 ## assigning csv to dataframe
 nhl_data = pd.read_csv('/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/CSVs/NHL_2022_2023_skaters.csv')
@@ -62,13 +64,38 @@ X_pca = pca.fit_transform(X_scaled)
 #--------------------------Uncomment if you want elbow method for optimal K---------------------------------------------------
 ## elbow method for optimal K
 # inertia = []
-# for k in range (1,11): ## update thise depending on number of features
+# for k in range (1,8): ## update thise depending on number of features
 #     kmeans = KMeans(n_clusters=k, random_state=42)
 #     kmeans.fit(X_pca)
 #     inertia.append(kmeans.inertia_)
 
 # ## plotting elbow results
-# plt.plot(range(1, 11), inertia, marker='o') ## update thise depending on number of features
+# plt.plot(range(1, 8), inertia, marker='o') ## update thise depending on number of features
 # plt.xlabel('Number of Clusters (K)')
 # plt.ylabel('Inertia')
 # plt.show()
+
+
+#--------------------------Uncomment if you want silhouette method for optimal K---------------------------------------------------
+# # Determine optimal K using silhouette score
+# max_k = 10  # Set the maximum number of clusters to consider
+# best_score = float('-inf')  # Initialize with negative infinity to ensure any valid score is an improvement
+# best_k = 0
+
+# for k in range(2, max_k + 1):
+#     kmeans = KMeans(n_clusters=k, random_state=42)
+#     labels = kmeans.fit_predict(X_scaled)
+#     score = silhouette_score(X_scaled, labels)
+    
+#     print(f"For K={k}, silhouette score: {score}")
+
+optimal_k = 4
+
+## KMeans clustering
+kmeans = KMeans(n_clusters=optimal_k, random_state=42)
+nhl_data['cluster'] = kmeans.fit_predict(X_pca)
+
+cluster_stats = nhl_data.drop('playerId', axis=1).groupby('cluster').mean()
+cluster_stats = cluster_stats.round(2)
+print(cluster_stats)
+
