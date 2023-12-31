79d822fcdfea5074090afb96e4b79c1ec2e98d27
JHutch89 <JasonCHutches@gmail.com>
2023-12-30 22:05:38 -0500

    created golf app

---
+++ Projects/Golf App/Code/Golf App.py
@@ -0,0 +1,27 @@
+import pandas as pd
+import streamlit as st
+
+sjmga_2023 = pd.read_csv("/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/Projects/Golf App/CSV/sjmga_2023.csv")
+sjmga_2023 = sjmga_2023.fillna("")
+sjmga_2023.set_index("Golfer", inplace=True)
+
+# Streamlit App
+st.title("Golfer Data App")
+
+# Create a dropdown for selecting a golfer
+selected_golfer = st.selectbox("Select Golfer", sjmga_2023.index)
+
+# Filter the DataFrame based on the selected golfer
+filtered_data = sjmga_2023.loc[selected_golfer]
+filtered_data = filtered_data.drop(columns=["Index"])
+# Double the "Course Score" where "Course" is "Makefield Highlands"
+filtered_data.loc[filtered_data["Outing"] == "Makefield Highlands", "Course Score"] *= 2
+
+# Calculate and display the average score for the selected golfer
+average_score = round(filtered_data['Course Score'].mean(),0)
+st.write(f"Average Score for {selected_golfer}: {average_score}")
+
+# Display the filtered data
+st.write("Selected Golfer:", selected_golfer)
+st.write(filtered_data)
+
