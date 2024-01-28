197c0e34c5f9965ffe5e59d78523bd9b88d60325
JHutch89 <JasonCHutches@gmail.com>
2024-01-27 23:05:05 -0500

    made changes to repo structure

--- Projects/Golf App/Golf App.py
+++ Projects/Golf App/Golf App.py
@@ -1,16 +1,19 @@
 import pandas as pd
 import streamlit as st
 
-sjmga_2023 = pd.read_csv("/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/Projects/Golf App/CSV/sjmga_2023.csv")
+sjmga_2023 = pd.read_csv("/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/Projects/Golf App/sjmga_2023.csv")
 sjmga_2023 = sjmga_2023.fillna("")
 sjmga_2023.set_index("Golfer", inplace=True)
 
 # Streamlit App
 st.title("SJMGA 2023")
 
+<<<<<<< HEAD:Projects/Golf App/Code/Golf App.py
 # image_path = "Projects/Golf App/Images/golf ball hole.jpeg"
 # st.image(image_path, use_column_width=True)
 
+=======
+>>>>>>> af33fe5 (made changes to repo structure):Projects/Golf App/Golf App.py
 # Create a dropdown for selecting a golfer
 selected_golfer = st.selectbox("Select Golfer", sjmga_2023.index)
 
