042e21b8e0379268f76fa0435ff794fbe2afe449
JHutch89 <JasonCHutches@gmail.com>
2024-03-03 19:09:57 -0500

    updated dir

--- golf_main.py
+++ golf_main.py
@@ -8,7 +8,7 @@ st.write("")
 st.write("Welcome to the SJMGA app home page! Please click one of the links below to navigate to your desired destination. If you run into any issues, please reach out to Jason Hutches.")
 
 # Link to golf_cal.py
-st.page_link('golf_cal.py', label= "Schedule | Dues")
+st.page_link("/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/golf/page/golf_app.py", label= "Schedule | Dues")
 
 # Link to golf_app.py
-st.page_link('golf_app.py', label= "Scores | Handicaps | Winnings")
+st.page_link("/Users/jasonhutches/Desktop/Jason Hutches/Hutches_Repo/golf/page/golf_cal.py", label= "Scores | Handicaps | Winnings")
