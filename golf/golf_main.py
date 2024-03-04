56604fd802432b0013b7b7be2a64b9d6612188f6
JHutch89 <JasonCHutches@gmail.com>
2024-03-03 23:18:52 -0500

    deleted dev container

--- golf/golf_main.py
+++ golf/golf_main.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 st.title("SJMGA Home")
 image_path = "golf/golf_logo.jpg"
-st.image(image_path, use_column_width=True, width=250)
+st.image(image_path,  width=500)
 
 st.write("")
-st.write("Welcome to the SJMGA app home page! Please click one of the links below to navigate to your desired destination. If you run into any issues, please reach out
+st.write("Welcome to the SJMGA app home page! Please click one of the links below to navigate to your desired destination. If you run into any issues, please reach out to Jason Hutches.")
