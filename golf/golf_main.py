4e02a94738c045d5abb49062c881154a28cd4647
JHutch89 <JasonCHutches@gmail.com>
2024-03-04 10:58:24 -0500

    updated golf_scores

--- golf/golf_main.py
+++ golf/golf_main.py
@@ -6,4 +6,5 @@ st.image(image_path,  width=350)
 
 st.write("")
 st.write("""Welcome to the SJMGA app home page! Click on one of the tabs on the left-hand pane to access scores, handicaps and 2024 schedule.
-         If you are on a mobile device you wil
+         If you are on a mobile device you will need to click the > icon in the top left to access the pane. Please
+         reach out to Jason Hutches with any questions or feedback...and good luck this season!""")
