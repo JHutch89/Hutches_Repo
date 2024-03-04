97f21957b16bdf28f97e12ae4e70b6f8fe7aaaad
JHutch89 <JasonCHutches@gmail.com>
2024-03-04 08:30:41 -0500

    updated spelling

--- golf/pages/golf_cal.py
+++ golf/pages/golf_cal.py
@@ -6,7 +6,7 @@ calendar = [
     ["4/20/24", "Pinelands", "TBD", "TBD"],
     ["5/18/24", "Makefield Highlands", "TBD", "TBD"],
     ["6/29/24", "Harbor Pines", "TBD", "TBD"],
-    ["7/20/24", "Towne and Country", "TBD", "TBD"],
+    ["7/20/24", "Town and Country", "TBD", "TBD"],
     ["8/17/24", "Cream Ridge", "TBD", "TBD"],
     ["9/14/24", "Golden Pheasant", "TBD", "TBD"],
     ["10/12/24", "Mercer Oaks", "TBD", "TBD"],
