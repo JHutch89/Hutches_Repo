0c6b04baf44e3d5c4664e7afd26fb77de0fd1cca
JHutch89 <JasonCHutches@gmail.com>
2024-03-03 19:07:48 -0500

    updated dir

--- golf/golf_main.py
+++ golf/golf_main.py
@@ -1,8 +1,8 @@
 import streamlit as st
 
 st.title("SJMGA Home")
-image_path = "Golf App/golf_logo.jpg"
-st.image(image_path, width=400)
+# image_path = "golf_logo.jpg"
+# st.image(image_path, width = 200)
 
 st.write("")
 st.write("Welcome to the SJMGA app home page! Please click one of the links below to navigate to your desired destination. If you run into any issues, please reach out to Jason Hutches.")
@@ -11,4 +11,4 @@ st.write("Welcome to the SJMGA app home page! Please click one of the links belo
 st.page_link('golf_cal.py', label= "Schedule | Dues")
 
 # Link to golf_app.py
-st.page_link( 'golf_app.py', label= "Scores | Handicaps | Winnings")
+st.page_link('golf_app.py', label= "Scores | Handicaps | Winnings")
