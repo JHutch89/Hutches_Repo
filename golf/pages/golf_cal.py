f0b32eed8df94b45317096226b1d61b62109a51d
JHutch89 <JasonCHutches@gmail.com>
2024-03-30 19:47:25 -0400

    added pinelands fees and times

--- golf/pages/golf_cal.py
+++ golf/pages/golf_cal.py
@@ -3,11 +3,11 @@ import streamlit as st
 from datetime import datetime
 
 calendar = [
-    ["4/20/24", "Pinelands", "TBD", "TBD"],
+    ["4/20/24", "Pinelands", "$89", "9:33am"],
     ["5/18/24", "Makefield Highlands", "TBD", "TBD"],
     ["6/29/24", "Harbor Pines", "TBD", "TBD"],
     ["7/20/24", "Town and Country", "TBD", "TBD"],
-    ["8/10/24", "Cream Ridge", "TBD", "TBD"],
+    ["8/10s/24", "Cream Ridge", "TBD", "TBD"],
     ["9/14/24", "Golden Pheasant", "TBD", "TBD"],
     ["10/12/24", "Mercer Oaks", "TBD", "TBD"],
     ["11/2/24", "Scotland Run *", "TBD", "TBD"]
