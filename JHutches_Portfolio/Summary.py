a4960d61dc474bf65e9026ff50f1b96a33fab549
JHutch89 <JasonCHutches@gmail.com>
2024-09-02 20:45:36 -0400

    metric mind addition

--- JHutches_Portfolio/Summary.py
+++ JHutches_Portfolio/Summary.py
@@ -7,8 +7,8 @@ st.title("Jason Hutches' Portfolio")
 # About Me section
 st.header("Get to Know Me...")
 st.write("""
-Hi!  I'm Jason Hutches, father of two wonderful children and husband to the most amazing wife. I'm a data data scientist by trade, but 
- off the clock you'll find me coaching my daughter's soccer team, operating a golf league and spending time with family.
+Hi!  I'm Jason Hutches, father of two wonderful children and husband to the most amazing wife. I'm a data scientist/analyst by trade, but 
+ off the clock you'll find me coaching my daughter's soccer team, running a golf league and spending time with family.
    I'm a huge soccer and hockey fan (go Chelsea and Devils!), and enjoy sharing that passion with my kids.
  I have a deep enthusiasm for math/data and am always eager to expand my knowledge. Life is good, and every day presents a new opportunity to learn and grow!
          """)  
---
+++ JHutches_Portfolio/pages/Out_of_Box_Stats.py
@@ -0,0 +1,97 @@
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+st.image("Out_of_Box_Stats/metric_mind.jpeg", width=300)
+st.write("")
+st.write("""
+        Metric Mind is an out-of-the-box statistics generator that provides instant statistical insights into your 
+        data with minimal effort. Simply drag, drop, or upload your CSV file, and let Metric Mind do the rest. 
+        The tool automatically generates the following:
+        - Summary Statistics
+        - Box and Whisker plot with dimension selection and outlier integration
+        - Correlation Matrix
+        - Missing Data Counts
+        - Various Histograms 
+""")
+st.write("")
+
+uploaded_file = st.file_uploader("Choose a CSV file", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+
+    # Convert date columns to datetime format
+    for col in df.columns:
+        if df[col].dtype == 'object':
+            try:
+                df[col] = pd.to_datetime(df[col])
+            except ValueError:
+                continue
+
+    st.write("File successfully uploaded!")
+    st.write(df.head()) 
+    st.write("")
+
+    st.write("### Basic Statistics")
+    
+    st.write("#### Summary Statistics")
+    st.write(df.describe())
+    st.write("")
+
+    numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
+    st.write("### Box and Whisker Plot")
+    selected_column = st.selectbox("Select a column for Box and Whisker Plot:", numerical_columns)
+
+
+    if selected_column:
+     st.write(f"Box and Whisker Plot for {selected_column}")
+
+    # Generate the horizontal boxplot
+    plt.figure()
+    data = df[selected_column].dropna()
+
+    # Create the boxplot
+    boxplot = plt.boxplot(data, patch_artist=True, vert=False)
+
+    plt.ylabel(selected_column)
+    plt.title(f'Box and Whisker Plot of {selected_column}')
+
+    # Extract the statistics manually
+    Q1 = np.percentile(data, 25)
+    Q3 = np.percentile(data, 75)
+    median = np.median(data)
+    min_whisker = np.min(data[data >= Q1 - 1.5 * (Q3 - Q1)])
+    max_whisker = np.max(data[data <= Q3 + 1.5 * (Q3 - Q1)])
+
+    # Annotate the plot with statistics below the boxplot
+    y_positiona = .65 
+    y_positionb = .75
+    plt.text(median, y_positionb, f'Median: {median:.2f}', horizontalalignment='center')
+    plt.text(Q1, y_positiona, f'Q1: {Q1:.2f}', horizontalalignment='center')
+    plt.text(Q3, y_positiona, f'Q3: {Q3:.2f}', horizontalalignment='center')
+    plt.text(min_whisker, y_positionb, f'Min: {min_whisker:.2f}', horizontalalignment='center')
+    plt.text(max_whisker, y_positionb, f'Max: {max_whisker:.2f}', horizontalalignment='center')
+
+    st.pyplot(plt)
+    st.write("")
+        
+    st.write("#### Correlation Matrix")
+    # Exclude non-numerical columns for correlation matrix
+    numerical_df = df.select_dtypes(include=['float64', 'int64'])
+    st.write(numerical_df.corr())
+    
+    st.write("#### Missing Data")
+    st.write(df.isnull().sum())
+    
+    st.write("#### Histograms")
+    numerical_columns = df.select_dtypes(include=['float64', 'int64']).columns
+    for col in numerical_columns:
+        st.write(f"Histogram for {col}")
+        plt.figure()
+        plt.hist(df[col].dropna(), bins=20, edgecolor='k')
+        plt.xlabel(col)
+        plt.ylabel('Counts')  # Change the y-axis label to "Counts"
+        plt.title(f'Histogram of {col}')
+        st.pyplot(plt)  # Display the histogram in Streamlit
