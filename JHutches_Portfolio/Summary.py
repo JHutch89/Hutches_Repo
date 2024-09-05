da751b52bfb8ef2af2e3d4f1b0a53017e5ea0afb
JHutch89 <JasonCHutches@gmail.com>
2024-09-04 22:20:28 -0400

    updated summary

--- JHutches_Portfolio/Summary.py
+++ JHutches_Portfolio/Summary.py
@@ -7,17 +7,21 @@ st.title("Jason Hutches' Portfolio")
 # About Me section
 st.header("Get to Know Me...")
 st.write("""
-Hi!  I'm Jason Hutches, father of two wonderful children and husband to the most amazing wife. I'm a data scientist/analyst by trade, but 
- off the clock you'll find me coaching my daughter's soccer team, running a golf league and spending time with family.
-   I'm a huge soccer and hockey fan (go Chelsea and Devils!), and enjoy sharing that passion with my kids.
- I have a deep enthusiasm for math/data and am always eager to expand my knowledge. Life is good, and every day presents a new opportunity to learn and grow!
+Hi!  I'm Jason Hutches, proud father of two wonderful children and husband to the most amazing wife. I'm a data scientist/analyst by trade, but 
+off the clock you'll find me coaching my daughter's soccer team, running a golf league and spending time with family.
+I'm a huge soccer and hockey fan (go Chelsea and Devils!), and enjoy sharing that enthusiasm with my kids.
+I have a deep passion for math and data, and I am always on the lookout for new ways to learn, grow and apply that knowledge.
+Every day is a chance to connect the dots—whether in data or in life—and I’m here for the ride!
          """)  
 
 # Professional Summary
 st.header("Professional Summary")
 professional_summary = """
-Accomplished Data Analytics professional with over a decade of experience in managing teams and enhancing operational processes across diverse industries including eCommerce, logistics, client relations, and compliance. 
-Expert in leveraging statistical models and data to drive strategic growth and operational excellence. Known for a robust ability to swiftly adapt, master, and implement solutions that consistently achieve improved business outcomes.
+Accomplished data professional with over 10 years of experience in analytics, data science, 
+and business intelligence. Proven track record in developing and implementing comprehensive 
+BI solutions and strategic plans, driving business success. Skilled in mentoring and developing 
+high-performing teams and effectively communicating strategic insights to stakeholders. Experienced 
+in Agile delivery and program management.
 """
 st.write(professional_summary)
 
