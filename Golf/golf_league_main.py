0a4903359e137f1bd2c0ff798abbc1e930582b8d
JHutch89 <JasonCHutches@gmail.com>
2025-03-09 22:57:44 -0400

    updated date

--- Golf/golf_league_main.py
+++ Golf/golf_league_main.py
@@ -5,6 +5,6 @@ image_path = "Golf/golf_logo.jpg"
 st.image(image_path,  width=350)
 
 st.write("")
-st.write("""Welcome to the SJMGA app home page! Click on one of the tabs on the left-hand pane to access scores, handicaps and 2024 schedule.
+st.write("""Welcome to the SJMGA app home page! Click on one of the tabs on the left-hand pane to access scores, handicaps and 2025 schedule.
          If you are on a mobile device you will need to click the > icon in the top left to access the pane. Please
          reach out to Jason Hutches with any questions or feedback...and good luck this season!""")
